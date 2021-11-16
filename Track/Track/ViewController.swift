0759cf9dcc7975ac714cac46d26877bb622d06fd
Christopher Yecora <cey21b@my.fsu.edu>
2021-11-15 19:33:46 -0500

    Add features to Pomodoro timer

--- Track/Track/ViewController.swift
+++ Track/Track/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func shortAction(_ sender: Any) {
+        timerLabel.text = "5:00"
+        textLabel.text = "Relax...for now"
+        
+    }
+    
+    @IBAction func pomoAction(_ sender: Any) {
+        timerLabel.text = "25:00"
+        textLabel.text = "Focus!"
+    }
+    
+    
+    @IBAction func longAction(_ sender: Any) {
+        timerLabel.text = "10:00"
+        textLabel.text = "Relax...for now"
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
