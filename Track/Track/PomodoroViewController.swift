817a851af6f24223fe91d4c8909b228976aa4341
Daniel Williams <danielfrwilliams@gmail.com>
2021-11-29 22:31:04 -0500

    Fix Pomodoro timer and redesign Pomodoro view

--- Track/Track/PomodoroViewController.swift
+++ Track/Track/PomodoroViewController.swift
@@ -11,26 +11,28 @@ import Foundation
 import AVFoundation
 import AudioToolbox
 
-class PomodoroViewController: UIViewController {
+enum MODE {
+    case POMODORO
+    case SHORT_BREAK
+    case LONG_BREAK
+}
 
-    
+class PomodoroViewController: UIViewController {
     
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     var timer = Timer()
     var isTimerRunning = false
-    var resumeTapped = false
-    var seconds = 0
-    var pomoTapped = false
-    var shortTapped = false
-    var longTapped = false
+    var seconds = 1500
+    var timerMode:MODE = MODE.POMODORO
     
     func runTimer() {
         isTimerRunning = true
         timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(PomodoroViewController.updateTimer)), userInfo: nil, repeats: true)
-        pauseButton.isEnabled = true
     }
     
     @objc func updateTimer() {
@@ -50,48 +52,31 @@ class PomodoroViewController: UIViewController {
         if isTimerRunning == false {
             runTimer()
             self.startButton.isEnabled = false
+            self.pauseButton.isEnabled = true
         }
     }
     
     
     @IBAction func pauseButtonTapped(_ sender: Any) {
-        if self.resumeTapped == false {
+        if isTimerRunning == true {
             timer.invalidate()
             isTimerRunning = false
-            self.resumeTapped = true
-            self.pauseButton.setTitle("RESUME", for: .normal)
-            pauseButton.setTitleColor(UIColor .white, for: .normal)
-        }
-        else {
-            runTimer()
-            self.resumeTapped = false
-            isTimerRunning = true
-            self.pauseButton.setTitle("PAUSE", for: .normal)
-            pauseButton.setTitleColor(UIColor .white, for: .normal)
+            self.pauseButton.isEnabled = false
+            self.startButton.isEnabled = true
         }
     }
     
     
     @IBAction func resetButtonTapped(_ sender: Any) {
         timer.invalidate()
-        if shortTapped == true {
-            shortTapped = true
-            pomoTapped = false
-            longTapped = false
+        switch timerMode {
+        case MODE.SHORT_BREAK:
             seconds = 300
             timerLabel.text = timeString(time: TimeInterval(seconds))
-        }
-        else if longTapped == true {
-            shortTapped = false
-            pomoTapped = false
-            longTapped = true
+        case MODE.LONG_BREAK:
             seconds = 600
             timerLabel.text = timeString(time: TimeInterval(seconds))
-        }
-        else if pomoTapped == true {
-            shortTapped = false
-            pomoTapped = true
-            longTapped = false
+        case MODE.POMODORO:
             seconds = 1500
             timerLabel.text = timeString(time: TimeInterval(seconds))
         }
@@ -102,20 +87,15 @@ class PomodoroViewController: UIViewController {
     
     
     @IBAction func shortAction(_ sender: Any) {
-        shortTapped = true
-        pomoTapped = false
-        longTapped = false
+        timerMode = MODE.SHORT_BREAK
         seconds = 300
         let time = timeString(time: 300)
         timerLabel.text = time
         textLabel.text = "Relax...for now"
-        
     }
     
     @IBAction func pomoAction(_ sender: Any) {
-        shortTapped = false
-        pomoTapped = true
-        longTapped = false
+        timerMode = MODE.POMODORO
         seconds = 1500
         let time = timeString(time: 1500)
         timerLabel.text = time
@@ -124,9 +104,7 @@ class PomodoroViewController: UIViewController {
     
     
     @IBAction func longAction(_ sender: Any) {
-        shortTapped = false
-        pomoTapped = false
-        longTapped = true
+        timerMode = MODE.LONG_BREAK
         seconds = 600
         let time = timeString(time: 600)
         timerLabel.text = time
@@ -144,7 +122,12 @@ class PomodoroViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        timerLabel.text = timeString(time: 1500)
+        startButton.layer.cornerRadius = 20
+        pauseButton.layer.cornerRadius = 20
+        resetButton.layer.cornerRadius = 20
         pauseButton.isEnabled = false
+        
     }
 
 
